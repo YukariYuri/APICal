1450cb7384cc58cfed4e60403267775de8108c84
YukariYuri <tonnamhttp@gmail.com>
2026-06-27 20:07:07 +0700

    update operator ให้โค้ดสั้นขึ้น

--- API.js
+++ API.js
@@ -4,22 +4,12 @@ async function Cal() {
     a = document.getElementById("num1").value;
     b = document.getElementById('num2').value;
     op = document.getElementById("operator").value;
-
-    switch (op) {
-        case "+": op = "plus"; break;
-        case "-": op = "minus"; break;
-        case "*": op = "times"; break;
-        case "/": op = "over"; break;
-    }
-
+    
     if (isNaN(a) || isNaN(b) || a === "" || b === "") 
         return alert("Number invalid or exceeds limit.");
     
     const response = await fetch(`${ApiPort}/Cal?a=${a}&b=${b}&op=${op}`)
         .then(res => res.json())
-        .catch(err => {
-            return {error : err};
-        })
 
     if (!response.error) {
         const result_tag = document.getElementById("result");
