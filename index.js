0009d3c3495fba1bcd06e251c4a25e2ca5af33db
YukariYuri <tonnamhttp@gmail.com>
2026-06-28 13:12:19 +0700

    แทนที่ let ใน BE ด้วย const

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ app.use(cors())
 const Port = 3000
 
 app.get('/plus', (req, res) => {
-    let a = parseFloat(req.query.a);
-    let b = parseFloat(req.query.b);
-    let result = a + b;
+    const a = parseFloat(req.query.a);
+    const b = parseFloat(req.query.b);
+    const result = a + b;
     return res.json({
         "Result" : result
     })
 })
 
 app.get('/minus', (req, res) => {
-    let a = parseFloat(req.query.a);
-    let b = parseFloat(req.query.b);
-    let result = a - b;
+    const a = parseFloat(req.query.a);
+    const b = parseFloat(req.query.b);
+    const result = a - b;
     return res.json({
         "Result" : result
     })
 })
 
 app.get('/times', (req, res) => {
-    let a = parseFloat(req.query.a);
-    let b = parseFloat(req.query.b);
-    let result = a * b;
+    const a = parseFloat(req.query.a);
+    const b = parseFloat(req.query.b);
+    const result = a * b;
     return res.json({
         "Result" : result
     })
 })
 
 app.get('/over', (req, res) => {
-    let a = parseFloat(req.query.a);
-    let b = parseFloat(req.query.b);
+    const a = parseFloat(req.query.a);
+    const b = parseFloat(req.query.b);
     if ((a || a === 0) && b === 0) return res.json({ error : "The value is cannot be determined."});
-    let result = a / b;
+    const result = a / b;
     return res.json({
         "Result" : result
     })
